d42cf308ae8f10502408b661b2e4d7add1fd8fd2
bdelucia <bdelucia@asu.edu>
2024-12-16 20:29:38 -0700

    working dom manip

--- script.js
+++ script.js
@@ -1,50 +1,73 @@
+/* Factory and IIFE (instantly invoked function expression) ticTacToe */
 const ticTacToe = (function () {
-    let board = [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""] 
+    let board = [ /* private */
+        ["_", "_", "_"],
+        ["_", "_", "_"],
+        ["_", "_", "_"] 
     ]
-    let currentPlayer = "X";
+    let currentPlayer = "X"; /* private */
 
+    /* Public functions */
+
+    /* Appends board with some | to show a grid in the webpage */
     function printBoard() {
-        console.log(board.map(row => row.join(" | ")).join("\n------\n"));
+        let gameBoard = document.querySelector(".gameBoard");
+        gameBoard.innerHTML = "";
+        board.forEach(row => {
+            const rowDiv = document.createElement("div"); 
+            rowDiv.textContent = row.join(" | "); 
+            gameBoard.appendChild(rowDiv); 
+        });
     }
 
+    /* Marks cells in the board based on row and col given, returns true if winning condition achieved with the move */
     function makeMove(row, col){
-        if(board[row][col] === ""){
+        if(board[row][col] === "_"){
             board[row][col] = currentPlayer;
+            printBoard()
             if(checkWin()){
-                console.log(`${currentPlayer} wins!`);
+                alert(`${currentPlayer} wins!`);
+                return true;
             }
-            currentPlayer = currentPlayer === "X" ? "O" : "X";
+            currentPlayer = currentPlayer === "X" ? "O" : "X"; /* Flips the player */
+            return false;
         } else {
-            console.log("Invalid move")
+            console.log("Invalid move");
+            return false;
         }
     }
 
+    /* Checks if there are any winning conditions in the board's current state */
     function checkWin() {
         const winningLines = [
-            ...board,
-            [board[0][0], board[1][0], board[2][0]],
-            [board[0][1], board[1][1], board[2][1]],
-            [board[0][2], board[1][2], board[2][2]],
-            [board[0][0], board[1][1], board[2][2]],
-            [board[0][2], board[1][1], board[2][0]]
+            ...board, /* the rows */
+            [board[0][0], board[1][0], board[2][0]], /* 1st column */
+            [board[0][1], board[1][1], board[2][1]], /* 2nd column */
+            [board[0][2], board[1][2], board[2][2]], /* 3rd column */
+            [board[0][0], board[1][1], board[2][2]],  /* diagonal "\" */
+            [board[0][2], board[1][1], board[2][0]] /* diagonal "/" */
         ];
-        return winningLines.some(line => line.every(cell => cell === currentPlayer));
+        return winningLines.some(line => line.every(cell => cell === currentPlayer)); /* checks if there is atleast one winning line with all the cells filled by one player */
     }
-
-    return { printBoard, makeMove }
+    return { printBoard, makeMove, checkWin } /* makes the functions publically available, since board and currentPlayer aren't included, they are closed off (example of closure) and made private */
 })();
 
-ticTacToe.printBoard();
-ticTacToe.makeMove(0, 0); // X
-ticTacToe.printBoard();
-ticTacToe.makeMove(0, 1); // O
-ticTacToe.printBoard();
-ticTacToe.makeMove(1, 1);
-ticTacToe.printBoard();
-ticTacToe.makeMove(0, 2);
-ticTacToe.printBoard();
-ticTacToe.makeMove(2, 2);
-ticTacToe.printBoard();
+const runGame = (function () {
+    ticTacToe.printBoard();
+    document.querySelector("#submitMove").addEventListener("click", () => {
+        ticTacToe.printBoard();
+        const row = parseInt(document.querySelector("#rowInput").value, 10);
+        const col = parseInt(document.querySelector("#colInput").value, 10);
+    
+        if (!isNaN(row) && !isNaN(col) && row >= 0 && row <= 2 && col >= 0 && col <= 2) {
+            const gameOver = ticTacToe.makeMove(row, col);
+            if (gameOver) {
+                document.querySelector("#submitMove").disabled = true; 
+            }
+        } else {
+            alert("Please enter valid row and column values (0-2).");
+        }
+    });
+})();
+
+
